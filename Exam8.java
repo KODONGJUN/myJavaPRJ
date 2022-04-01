922b74b308a34b12f3f26b48d845c0ed56c43c1d
KODONGJUN <101384421+KODONGJUN@users.noreply.github.com>
2022-04-01 16:48:59 +0900

    Add files via upload

---
+++ Exam8.java
@@ -0,0 +1,10 @@
+public class Exam8 {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a << 1);
+        System.out.println(a);
+        System.out.println(a << 2);
+        System.out.println(a << 3);
+        System.out.println(a >> 1);
+    }
+}
