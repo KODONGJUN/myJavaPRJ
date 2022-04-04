22abc94280c6fd0333fce09809c12a41e60b3763
KODONGJUN <101384421+KODONGJUN@users.noreply.github.com>
2022-04-04 12:32:46 +0900

    Add files via upload

---
+++ Ex_044.java
@@ -0,0 +1,14 @@
+public class Ex_044 {
+    public static void main(String[] args) {
+
+        int num = 1;
+        int sum = 0;
+
+        while( num <= 10) {
+
+            sum += num;
+            num++;
+        }
+        System.out.println("1부터 10 까지의 합은"+ sum +"입니다. ");
+    }
+}
