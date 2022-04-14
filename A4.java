af76cdde973f18e9f76235a1f7f1d1bda1287052
KODONGJUN <101384421+KODONGJUN@users.noreply.github.com>
2022-04-14 10:52:39 +0900

    Add files via upload

---
+++ A4.java
@@ -0,0 +1,14 @@
+public class A4 {
+    public static void main(String[] args) {
+        int hap = 0;
+        int i;
+
+        i = 0;
+        while ( i < 11){
+            hap = hap + i;
+            i++;
+        }
+        System.out.printf("1부터 10까지의 합: %d \n", hap);
+
+    }
+}
---
+++ A7.java
@@ -0,0 +1,11 @@
+public class A7 {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 1; i < 101; i++) {
+            System.out.printf("for 문을 %d회 실행했습니다.\n", i);
+            break;
+        }
+        System.out.printf("for문을 종료하였습니다.\n");
+    }
+}
